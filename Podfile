325bd6825c2af064d827f19279e5f8b02a6ff838
Kostya Bunsberry <kostya.bunsberry@gmail.com>
2021-06-30 23:35:32 +0700

    Adding other files

---
+++ Podfile
@@ -0,0 +1,8 @@
+target 'HSESchoolVIII' do
+  use_frameworks!
+
+  pod 'Alamofire'
+  pod 'Realm', '5.2.0'
+  pod 'RealmSwift', '5.2.0'
+  pod 'Kingfisher'
+end
---
+++ Pods/Target Support Files/Pods-HSESchoolVIII/Pods-HSESchoolVIII-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HSESchoolVIII : NSObject
+@end
+@implementation PodsDummy_Pods_HSESchoolVIII
+@end
